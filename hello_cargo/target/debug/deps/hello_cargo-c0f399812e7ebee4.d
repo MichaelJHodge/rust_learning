65d9626730fe4f0abe049c15912929049b51de7b
michaelhodge <michaelhodge20@gmail.com>
2022-06-13 13:34:08 -0400

    cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-c0f399812e7ebee4.d
@@ -0,0 +1,5 @@
+/Users/michaelhodge/Desktop/Development/rust/rust_learning/hello_cargo/target/debug/deps/hello_cargo-c0f399812e7ebee4: src/main.rs
+
+/Users/michaelhodge/Desktop/Development/rust/rust_learning/hello_cargo/target/debug/deps/hello_cargo-c0f399812e7ebee4.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/michaelhodge/Desktop/Development/rust/rust_learning/hello_cargo/target/debug/hello_cargo: /Users/michaelhodge/Desktop/Development/rust/rust_learning/hello_cargo/src/main.rs
