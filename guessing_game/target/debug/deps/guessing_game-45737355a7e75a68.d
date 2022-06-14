e673767c3854fd56046ae1606cec9b81d99409b0
michaelhodge <michaelhodge20@gmail.com>
2022-06-14 11:13:49 -0400

    cleanup

--- guessing_game/target/debug/deps/guessing_game-45737355a7e75a68.d
+++
@@ -1,5 +0,0 @@
-/Users/michaelhodge/Desktop/Development/rust/rust_learning/guessing_game/target/debug/deps/guessing_game-45737355a7e75a68: src/main.rs
-
-/Users/michaelhodge/Desktop/Development/rust/rust_learning/guessing_game/target/debug/deps/guessing_game-45737355a7e75a68.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/michaelhodge/Desktop/Development/rust/rust_learning/guessing_game/target/debug/guessing_game: /Users/michaelhodge/Desktop/Development/rust/rust_learning/guessing_game/src/main.rs
