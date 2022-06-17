034843391dc5845f62fc4f714d98381f52a8912c
michaelhodge <michaelhodge20@gmail.com>
2022-06-17 15:29:29 -0400

    lifetime example description added

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -12,6 +12,16 @@ fn main() {
 
 use std::fmt::Display;
 
+//This is the longest function from Listing 10-22 that returns 
+// the longer of two string slices. But now it has an extra parameter 
+// named ann of the generic type T, which can be filled in by any type 
+// that implements the Display trait as specified by the where clause. 
+// This extra parameter will be printed using {}, which is why the 
+// Display trait bound is necessary. Because lifetimes are a type of generic,
+//  the declarations of the lifetime parameter 'a and the generic type 
+//  parameter T go in the same list inside the angle brackets after the 
+//  function name.
+
 fn longest_with_an_announcement<'a, T>(
     x: &'a str,
     y: &'a str,
