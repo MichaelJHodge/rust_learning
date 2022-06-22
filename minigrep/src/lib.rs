1471db94fb0bbcee2926c9dde2f2664f3cc19073
michaelhodge <michaelhodge20@gmail.com>
2022-06-22 15:51:19 -0400

    minigrep project

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,134 @@
+use std::error::Error;
+
+//Allows us to handle files.
+use std::fs;
+
+//Brings the env module into scope which allows us to use functions
+//pertaining to environment variables.
+use std::env;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
+
+//We tell Rust that the data returned by the search function will live as long as
+//the data passed into the search function in the contents argument. This is important!
+//The data referenced by a slice needs to be valid for the reference to be valid; if the
+//compiler assumes we’re making string slices of query rather than contents, it will do
+//its safety checking incorrectly.
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+//Basically the same as the above function, but instead we lowercase the query and
+//each line so whatever the case of the input args, they will be the same case when we check
+//if the lines contain the query.
+
+//When we pass query as an argument to the contains method now, we need to add an
+//ampersand because the signature of contains is defined to take a string slice.
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+//This means that the trait object Box<dyn Error> will return a type that
+//implements the Error trait, but we do not need to specify what type of error
+//the turn value will be.
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // --snip--
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+//Borrows these values (args) from main. In this case we clone the values
+//and make our own copies, which is more expensive from a performance standpoint
+//but it also means we don't need to keep track of reference lifetimes and in this case
+//it makes sense to sacrifice for some simplicity.
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        if args.len() > 3 {
+            return Err("Too many arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        //Keeps track of the new env variable we want. The is_err method checks whether or not
+        //it's an error and therefore unset - meaning we should do a case-sensitive search. If it is
+        //set to anything, it returns false and the progrma performs a case-insensitive search as usual.
+        //we dont need the value of our env variable, we just need to see if it's set or unset.
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
---
+++ minigrep/src/main.rs
@@ -0,0 +1,33 @@
+//Minigrep Rust Project//
+
+//This function from the std lib returns an iterator of the CLI args
+//that were given to minigrep. we use two :: because the function is
+//nested in more than one module.
+use std::env;
+use std::process;
+
+//This brings the Config type from the library crate into the binary crate's scope.
+
+use minigrep::Config;
+
+fn main() {
+    //This uses collect to turn the iterator into a vector containing
+    //all the values produced by the iterator. We specify that we
+    //want to create a collection specifically of strings since there are many
+    //types of collections we could create.
+
+    //Updated to handle the Result type
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    //We use if let to check whther run returns an Err value or not and exit if
+    //we do.
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
