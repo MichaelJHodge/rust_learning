d89fc997ade78b6639d1127468b84ea830690449
michaelhodge <michaelhodge20@gmail.com>
2022-06-27 09:31:01 -0400

    concurrency, smart pointers

---
+++ channel/src/main.rs
@@ -0,0 +1,49 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // --snip--
+
+    let (tx, rx) = mpsc::channel();
+
+    //This clones the transmittering thread but still
+    //sends values to the same rx receiver. We call clone on the sending end
+    // of the channel, which gives us a new sending handle we can pass to the first
+    //spawned thread. This guves us two threads, each sending different
+    //messages to the receiver.
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+
+    // --snip--
+}
